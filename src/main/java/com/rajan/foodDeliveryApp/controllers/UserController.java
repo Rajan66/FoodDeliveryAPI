b391536e8664abd857893b1996927aaad6f3213a
Rajan66 <rajanmaharjan042@gmail.com>
2024-08-12 02:05:43 +0545

    fix: line fix

--- src/main/java/com/rajan/foodDeliveryApp/controllers/UserController.java
+++ src/main/java/com/rajan/foodDeliveryApp/controllers/UserController.java
@@ -4,21 +4,15 @@ import com.rajan.foodDeliveryApp.domain.dto.UserDto;
 import com.rajan.foodDeliveryApp.domain.entities.UserEntity;
 import com.rajan.foodDeliveryApp.mappers.Mapper;
 import com.rajan.foodDeliveryApp.mappers.impl.UserMapperImpl;
-import com.rajan.foodDeliveryApp.repositories.UserRepository;
 import com.rajan.foodDeliveryApp.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/api")
